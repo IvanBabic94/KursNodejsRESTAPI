9b464820ae923bb46e7b95ea016c2f5121bb3376
IvanBabic94 <babsonno2@gmail.com>
2024-07-26 15:52:59 +0200

    drugi post

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,7 +4,7 @@ exports.getPosts = (req, res, next) => {
     });
 };
 
-exports.postPost = (req,res,next) => {
+exports.createPost = (req,res,next) => {
     const title = req.body.title;
     const content = req.body.content;
     // Create post in db
